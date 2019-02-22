78fd9c9ae053cd4bbf68248883981336b9a816c1
김효상 <hyosang81@naver.com>
2019-02-22 19:26:09 +0900

    hyo

--- src/main/java/com/bestiansoft/pdfgen/controller/DocController.java
+++ src/main/java/com/bestiansoft/pdfgen/controller/DocController.java
@@ -32,10 +32,14 @@ public class DocController {
     public Map<String, Object> getDoc(@PathVariable String docId) {
         // Doc doc = docService.getDoc(docId);
         
+        System.out.println("생성자용 pdf 조회");
+
         Map<String, Object> ret = new HashMap<>();
 
         // ret.put("doc", doc.getFilePath());
-        ret.put("doc", "http://13.209.43.245:8080/sample.pdf");
+        //ret.put("doc", "http://13.209.43.245:8080/sample.pdf");        
+        ret.put("doc", "http://localhost:8888/sample.pdf");        
+        // ret.put("doc", "/sample.pdf");
         ret.put("signers", Signer.signers);
 
         return ret;
@@ -66,18 +70,38 @@ public class DocController {
         return ret;
     }
 
-    // 4. 생성자 및 참여자 서명 완료
+    // 4. 생성자 및 참여자 서명 완료 - 작업중
+    @RequestMapping(value = "/v1/document/{docId}/signer/{signerNo}", method = { RequestMethod.POST })
+    public Map<String, Object> getDocToSign(@PathVariable String docId, @PathVariable String signerNo, @RequestBody Doc inputDoc) {
+        
+        System.out.println("inputDoc : " + inputDoc.getDocId());
+        // Doc doc = docService.getDoc(docId);
 
+        Map<String, Object> ret = new HashMap<>();
+        // ret.put("doc", doc.getFilePath());
+        // ret.put("inputs", doc.getElements());
+        // ret.put("signer", Signer.getSigner(signerNo));
+
+        return ret;
+    }
     
-    // 5. 생성자 pdf 확인 화면    
-    // ex) @RequestMapping(value = "/v1/document/{docId}/signerAll/", method = { RequestMethod.GET })
+    /**
+     * 5. 생성자가 최종 pdf 확인 화면
+     *  - 문서아이디로 문서를 조회
+     *  - 생성/참여자 엘리먼트들을 모두 보여줌(수정은 안되고..)
+     */    
+    @RequestMapping( value="/v1/document/{docId}/signComplete/{signerId}", method= {RequestMethod.GET} )
+    public @ResponseBody PdfResponse contractView(@PathVariable String docId){        
+        //return docService.getDoc(docId);
+        return null;
+    }
 
 
     // 6. 생성자 pdf 작성 완료
     //@RequestMapping( value="/v1/document/{docId}/signer/{signerId}", method= {RequestMethod.POST} )    
-    @RequestMapping( value="/v1/document/{docId}/signComplete/{signerId}", method= {RequestMethod.GET} )
+    @RequestMapping( value="/v1/document/{docId}/signComplete/{signerId}", method= {RequestMethod.POST} )
 	//public @ResponseBody String req(@RequestBody Doc doc) {
-    public @ResponseBody PdfResponse req() {
+    public @ResponseBody PdfResponse signComplete() {
         //log.info("req called");
         System.out.println("pdf 생성");
         
